dcac5878ffc642a5534ef491eb6dd6597b3d3ddf
MantaHonk <135276525+MantaHonk@users.noreply.github.com>
2025-05-24 14:21:02 -0700

    Update views.py

--- website/views.py
+++ website/views.py
@@ -31,5 +31,6 @@ def delete_note():
         if note.user_id == current_user.id:
             db.session.delete(note)
             db.session.commit()
+            flash('Note Deleted!', category = 'success')
     
     return jsonify({})
